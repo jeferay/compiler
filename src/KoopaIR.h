754f1f7c6d6111572f869ccc7dc55194facc04d3
wzr <844570862@qq.com>
2022-06-11 17:35:36 +0800

    Update KoopaIR.h

--- src/KoopaIR.h
+++ src/KoopaIR.h
@@ -9,18 +9,32 @@ using namespace std;
 
 string get_binary_riscv(int is_Reg, int val);
 // 函数重载
-void Visit(const koopa_raw_slice_t &slice, char * RiscV);
+
 void Visit(const koopa_raw_return_t & ret, char * RiscV);
 void Visit(const koopa_raw_integer_t &integer, char * RiscV);
 void Visit(const koopa_raw_binary_t &binary, char * RiscV);
+void Visit(const koopa_raw_global_alloc_t &global_alloc, char *RiscV);
+void Visit(const koopa_raw_load_t &load, char*RiscV);
+void Visit(const koopa_raw_store_t &store,char*RiscV);
+
 void Visit(const koopa_raw_value_t &value, char * RiscV);
+int CalMemory(const koopa_raw_value_t &func);
+
 void Visit(const koopa_raw_basic_block_t &bb, char * RiscV);
+int CalMemory(const koopa_raw_basic_block_t &func);
+
 void Visit(const koopa_raw_function_t &func, char * RiscV);
+int CalMemory(const koopa_raw_function_t &func);
+
 void Visit(const koopa_raw_slice_t &slice, char * RiscV);
+int CalMemory(const  koopa_raw_slice_t &slice);
+
 void Visit(const koopa_raw_program_t &program, char * RiscV);
 void Str_2_KoopaIR(const char * IR, char * RiscV);
 
+
 #define MAX_REG 15
+int S=0;
 
 // binary的顺序是lhs在栈顶，rhs在第二个位置，先左再右，因为计算的顺序是先右再左
 struct Reg_Stack{
@@ -63,29 +77,31 @@ struct Reg_Stack{
   }
 } reg_stack;
 
+//保存指针的偏移量
+std::map<koopa_raw_value_t,int> func_stack;
 
 // 访问return指令
 void Visit(const koopa_raw_return_t & ret, char * RiscV)
 {
+  string temp_RiscV="";
   koopa_raw_value_t value = ret.value;// 定义为一个const koopa_raw_value_data_t *
   if (value != NULL)
   {
     koopa_raw_value_kind_t kind = value->kind;
     if (kind.tag==KOOPA_RVT_INTEGER){
-      strcat(RiscV, "  li a0, ");
-      int32_t ret_data = kind.data.integer.value;
-      strcat(RiscV, const_cast<char *>(to_string(ret_data).c_str()));
+      temp_RiscV = "  li a0, " + to_string(kind.data.integer.value) + "\n";
     }
-    else if (kind.tag==KOOPA_RVT_BINARY){
-      strcat(RiscV, "  mv a0, ");
-      int reg = reg_stack.pop();
-      strcat(RiscV, const_cast<char*>(get_binary_riscv(true,reg).c_str()));
-      reg_stack.set_available();
+    else if (kind.tag==KOOPA_RVT_LOAD||kind.tag==KOOPA_RVT_BINARY ){
+      temp_RiscV = "  lw a0, " + to_string(func_stack[value]) + "(sp)\n";
     }
-    strcat(RiscV, "\n");
   }
-  strcat(RiscV, "  ret\n");
+  
+  temp_RiscV+="  addi sp, sp, " + to_string(S) + "\n  ret\n";
+  strcat(RiscV, const_cast<char*>(temp_RiscV.c_str()));
+  return;
 }
+
+
 
 // get the riscv-register according to the val
 string get_binary_riscv(int is_Reg, int val){
@@ -102,80 +118,60 @@ string get_binary_riscv(int is_Reg, int val){
 void Visit(const koopa_raw_binary_t &binary, char * RiscV)
 {
   string temp_RiscV = "";
-  int reg;
   //cout<<"visit_ins_binary in\n";
   koopa_raw_value_t lhs = binary.lhs; // *koopa_raw_value_data指针类型
   koopa_raw_value_t rhs = binary.rhs;
-
-  //先处理左右运算数，处理顺序是先又后左，因为入栈顺序是先左后右
-  int l_val=0,r_val=0;
-  bool is_Reg_l=false,is_Reg_r=false;
-
-  if (rhs->kind.tag==KOOPA_RVT_BINARY){
-    r_val = reg_stack.pop();
-    is_Reg_r = true;
+  string left="t0";
+  string right = "t1";
+  if(lhs->kind.tag==KOOPA_RVT_INTEGER){
+    if (lhs->kind.data.integer.value!=0){
+    temp_RiscV+="  li t0, " + to_string(lhs->kind.data.integer.value) + "\n";
+    }
+    else{
+      left = "x0";
+    }
   }
-  else if (rhs->kind.tag==KOOPA_RVT_INTEGER){
-    r_val = rhs->kind.data.integer.value;
-    is_Reg_r = false;
-  }
-  if (lhs->kind.tag==KOOPA_RVT_BINARY){
-    l_val = reg_stack.pop();
-    is_Reg_l=true;
-  }
-  else if (lhs->kind.tag==KOOPA_RVT_INTEGER){
-    l_val = lhs->kind.data.integer.value;
-    is_Reg_l = false;
-  }
-  
-  // load immediate
-  if (is_Reg_l==false&&l_val!=0){
-    reg = reg_stack.first_available();
-    reg_stack.set_alive(reg);
-    temp_RiscV+="  li   " + get_binary_riscv(true,reg) + ", " + to_string(l_val) + "\n";
-    is_Reg_l = true; // replace
-    l_val = reg;
-  }
-  if (is_Reg_r==false&&r_val!=0){
-    reg = reg_stack.first_available();
-    reg_stack.set_alive(reg);
-    temp_RiscV+="  li   " + get_binary_riscv(true,reg) + ", " + to_string(r_val) + "\n";
-    is_Reg_r = true; // replace
-    r_val = reg;
+  else if (lhs->kind.tag==KOOPA_RVT_LOAD || lhs->kind.tag==KOOPA_RVT_BINARY){
+    temp_RiscV+="  lw t0, " + to_string(func_stack[lhs])+"(sp)\n";
   }
 
-  //处理该条指令返回reg_stack
-  reg = reg_stack.first_available();
-  reg_stack.push(reg);
-
+  if(rhs->kind.tag==KOOPA_RVT_INTEGER){
+    if (rhs->kind.data.integer.value!=0){
+    temp_RiscV+="  li t1, " + to_string(rhs->kind.data.integer.value) + "\n";
+    }
+    else {
+      right = "x0";
+    }
+  }
+  else if (rhs->kind.tag==KOOPA_RVT_LOAD || rhs->kind.tag==KOOPA_RVT_BINARY){
+    temp_RiscV+="  lw t1, " + to_string(func_stack[rhs])+"(sp)\n";
+  }
   //cout<<binary.op<<" "<<(lhs->kind).data.integer.value<<" "<<(lhs->kind).tag<<" "<<(rhs->kind).data.integer.value<<" "<<(rhs->kind).tag<<endl;
   // 按照operator分类
   switch (binary.op) // binary.op是int类型的
   {
-  case KOOPA_RBO_NOT_EQ: temp_RiscV += "  xor   " + get_binary_riscv(true,reg) + ", " + get_binary_riscv(is_Reg_l,l_val) + ", " + get_binary_riscv(is_Reg_r,r_val) + "\n";
-                     temp_RiscV += "  snez   "+ get_binary_riscv(true,reg) + ", " + get_binary_riscv(true,reg) + "\n";break;
-  case KOOPA_RBO_EQ: temp_RiscV += "  xor   " + get_binary_riscv(true,reg) + ", " + get_binary_riscv(is_Reg_l,l_val) + ", " + get_binary_riscv(is_Reg_r,r_val) + "\n";
-                     temp_RiscV += "  seqz   "+ get_binary_riscv(true,reg) + ", " + get_binary_riscv(true,reg) + "\n";break;
-  case KOOPA_RBO_GT: temp_RiscV += "  sgt   " + get_binary_riscv(true,reg) + ", " + get_binary_riscv(is_Reg_l,l_val) + ", " + get_binary_riscv(is_Reg_r,r_val) + "\n";break;
-  case KOOPA_RBO_LT: temp_RiscV += "  slt   " + get_binary_riscv(true,reg) + ", " + get_binary_riscv(is_Reg_l,l_val) + ", " + get_binary_riscv(is_Reg_r,r_val) + "\n";break;
-  case KOOPA_RBO_GE: temp_RiscV += "  slt   " + get_binary_riscv(true,reg) + ", " + get_binary_riscv(is_Reg_l,l_val) + ", " + get_binary_riscv(is_Reg_r,r_val) + "\n";
-                     temp_RiscV += "  seqz   "+ get_binary_riscv(true,reg) + ", " + get_binary_riscv(true,reg) + "\n";break; 
-  case KOOPA_RBO_LE: temp_RiscV += "  sgt   " + get_binary_riscv(true,reg) + ", " + get_binary_riscv(is_Reg_l,l_val) + ", " + get_binary_riscv(is_Reg_r,r_val) + "\n";
-                     temp_RiscV += "  seqz   "+ get_binary_riscv(true,reg) + ", " + get_binary_riscv(true,reg) + "\n";break; 
-  case KOOPA_RBO_ADD:temp_RiscV += "  add   " + get_binary_riscv(true,reg) + ", " + get_binary_riscv(is_Reg_l,l_val) + ", " + get_binary_riscv(is_Reg_r,r_val) + "\n";break;
-  case KOOPA_RBO_SUB:temp_RiscV += "  sub   " + get_binary_riscv(true,reg) + ", " + get_binary_riscv(is_Reg_l,l_val) + ", " + get_binary_riscv(is_Reg_r,r_val) + "\n";break;
-  case KOOPA_RBO_MUL:temp_RiscV += "  mul   " + get_binary_riscv(true,reg) + ", " + get_binary_riscv(is_Reg_l,l_val) + ", " + get_binary_riscv(is_Reg_r,r_val) + "\n";break;
-  case KOOPA_RBO_DIV:temp_RiscV += "  div   " + get_binary_riscv(true,reg) + ", " + get_binary_riscv(is_Reg_l,l_val) + ", " + get_binary_riscv(is_Reg_r,r_val) + "\n";break;
-  case KOOPA_RBO_MOD:temp_RiscV += "  rem   " + get_binary_riscv(true,reg) + ", " + get_binary_riscv(is_Reg_l,l_val) + ", " + get_binary_riscv(is_Reg_r,r_val) + "\n";break;
-  case KOOPA_RBO_AND:temp_RiscV += "  and   " + get_binary_riscv(true,reg) + ", " + get_binary_riscv(is_Reg_l,l_val) + ", " + get_binary_riscv(is_Reg_r,r_val) + "\n";break;
-  case KOOPA_RBO_OR: temp_RiscV += "  or   "  + get_binary_riscv(true,reg) + ", " + get_binary_riscv(is_Reg_l,l_val) + ", " + get_binary_riscv(is_Reg_r,r_val) + "\n";break;
-
+  case KOOPA_RBO_NOT_EQ: 
+                     temp_RiscV += "  xor   t0, " + left + ", " + right + "\n";
+                     temp_RiscV += "  snez  t0, t0\n"; break;
+  case KOOPA_RBO_EQ: temp_RiscV += "  xor   t0, " + left + ", " + right + "\n";
+                     temp_RiscV += "  seqz  t0, t0\n"; break;
+  case KOOPA_RBO_GT: temp_RiscV += "  sgt   t0, " + left + ", " + right + "\n";break;
+  case KOOPA_RBO_LT: temp_RiscV += "  slt   t0, " + left + ", " + right + "\n";break;
+  case KOOPA_RBO_GE: temp_RiscV += "  slt   t0, " + left + ", " + right + "\n";
+                     temp_RiscV += "  seqz  t0, t0\n"; break;
+  case KOOPA_RBO_LE: temp_RiscV += "  sgt   t0, " + left + ", " + right + "\n";
+                     temp_RiscV += "  seqz  t0, t0\n"; break;
+  case KOOPA_RBO_ADD:temp_RiscV += "  add   t0, " + left + ", " + right + "\n";break;
+  case KOOPA_RBO_SUB:temp_RiscV += "  sub   t0, " + left + ", " + right + "\n";break;
+  case KOOPA_RBO_MUL:temp_RiscV += "  mul   t0, " + left + ", " + right + "\n";break;
+  case KOOPA_RBO_DIV:temp_RiscV += "  div   t0, " + left + ", " + right + "\n";break;
+  case KOOPA_RBO_MOD:temp_RiscV += "  rem   t0, " + left + ", " + right + "\n";break;
+  case KOOPA_RBO_AND:temp_RiscV += "  and   t0, " + left + ", " + right + "\n";break;
+  case KOOPA_RBO_OR: temp_RiscV += "  or    t0, " + left + ", " + right + "\n";break;
   default:
     break;
   }
   strcat(RiscV,const_cast<char*>(temp_RiscV.c_str()));
-  reg_stack.set_available(); // reset availability status
-
 }
 
 void Visit(const koopa_raw_integer_t &integer, char * RiscV)
@@ -184,11 +180,50 @@ void Visit(const koopa_raw_integer_t &integer, char * RiscV)
   
 }
 
+
+//访问alloc指令,分配栈，也就是map
+void Visit(const koopa_raw_global_alloc_t &global_alloc, char *RiscV){
+  koopa_raw_value_t init = global_alloc.init;//alloc的是init这个值
+  func_stack.insert(std::pair<koopa_raw_value_t,int>(init,func_stack.size()*4));
+  return;
+}
+void Visit(const koopa_raw_load_t &load, char*RiscV){
+  //load本身的
+  koopa_raw_value_t src = load.src;
+  std::string temp_RiscV = "  lw t0, " + to_string(func_stack[(src->kind).data.global_alloc.init])+"(sp)\n";
+  strcat(RiscV,const_cast<char*>(temp_RiscV.c_str()));
+  return;
+}
+void Visit(const koopa_raw_store_t &store,char*RiscV){
+  koopa_raw_value_t value = store.value;
+  const auto &kind = value->kind;
+  string temp_RiscV = "";
+  if (kind.tag==KOOPA_RVT_INTEGER)
+  {
+    int32_t ret_data = kind.data.integer.value;
+    temp_RiscV = "  li t0, " + to_string(ret_data) + "\n";
+  }
+  else if (kind.tag == KOOPA_RVT_LOAD || kind.tag==KOOPA_RVT_BINARY){
+    temp_RiscV = "  lw t0, " + to_string(func_stack[value]) + "(sp)\n";
+  }
+  koopa_raw_value_t dest = store.dest;
+  temp_RiscV +="  sw t0, " + to_string(func_stack[(dest->kind).data.global_alloc.init])+"(sp)\n";
+  strcat(RiscV,const_cast<char*>(temp_RiscV.c_str()));
+  return;
+}
+
+
+int CalMemory(const koopa_raw_value_t &value){
+  if(value->ty->tag==KOOPA_RTT_UNIT) return 0;
+  return 1;
+}
+
 // 访问value:指令或者是值
 void Visit(const koopa_raw_value_t &value, char * RiscV)
 {
   // 根据指令类型判断后续需要如何访问, 这里得到的kind是一个tag和data的struct
   const auto &kind = value->kind;
+  string temp_RiscV  = "";
   switch (kind.tag)
   {
     case KOOPA_RVT_RETURN:
@@ -200,15 +235,31 @@ void Visit(const koopa_raw_value_t &value, char * RiscV)
       Visit(kind.data.integer,RiscV);
       break;
     case KOOPA_RVT_BINARY:
-      Visit(kind.data.binary,RiscV);
-      
+      //同样是 返回值分配栈
+      func_stack.insert(std::pair<koopa_raw_value_t,int>(value,func_stack.size()*4));
+      Visit(kind.data.binary,RiscV); 
+      temp_RiscV = "  sw t0, " + to_string(func_stack[value]) + "(sp)\n";//直接以该value作为map key
+      strcat(RiscV,const_cast<char*>(temp_RiscV.c_str()));
       break;
-    default:
-      // 其他类型暂时遇不到
-      assert(false);
+    case KOOPA_RVT_ALLOC:
+      Visit(kind.data.global_alloc,RiscV);
+      break;//不用输出,但是要分配栈（用map保存）
+    case KOOPA_RVT_STORE:
+      Visit(kind.data.store,RiscV);
+      break;
+    case KOOPA_RVT_LOAD:
+      func_stack.insert(std::pair<koopa_raw_value_t,int>(value,func_stack.size()*4));
+      Visit(kind.data.load,RiscV);
+      temp_RiscV = "  sw t0, " + to_string(func_stack[value]) + "(sp)\n";//直接以该value作为map key
+      strcat(RiscV,const_cast<char*>(temp_RiscV.c_str()));
+      break;
+
   }
 }
 
+int CalMemory(const koopa_raw_basic_block_t &bb){
+  return CalMemory(bb->insts);
+}
 
 // 访问基本块
 void Visit(const koopa_raw_basic_block_t &bb, char * RiscV)
@@ -219,18 +270,49 @@ void Visit(const koopa_raw_basic_block_t &bb, char * RiscV)
   Visit(bb->insts,RiscV);// slice
 }
 
+int CalMemory(const koopa_raw_function_t &func){
+  return CalMemory(func->bbs);
+}
 // 访问函数
 void Visit(const koopa_raw_function_t &func, char * RiscV)
 {
-  // 执行一些其他的必要操作
-  // std::cout<<(func->name+1)<<":\n";
+  //首先要扫描所有的指令来获取需要的栈空间
+
   strcat(RiscV, (func->name+1));
   strcat(RiscV, ":\n");
   // 访问所有基本块
+  S = CalMemory(func) * 4;
+  S = (S/16) + !!(S%16);
+  S*=16;
+  string temp_RISCV = "  addi sp, sp, -" + to_string(S)+"\n";
+  strcat(RiscV,const_cast<char*>(temp_RISCV.c_str()));
   Visit(func->bbs,RiscV); //slice
 }
 
-
+int CalMemory(const koopa_raw_slice_t &slice){
+  int res=0;
+  for (size_t i = 0; i < slice.len; ++i) 
+  {
+    auto ptr = slice.buffer[i]; // 此时buffer中的元素是void*
+    // 根据 slice 的 kind 决定将 ptr 视作何种元素
+    
+    switch (slice.kind) 
+    {
+      case KOOPA_RSIK_BASIC_BLOCK:
+        // 计算基本块
+        res+=CalMemory(reinterpret_cast<koopa_raw_basic_block_t>(ptr));
+        break;
+      case KOOPA_RSIK_VALUE:
+        // 访问指令
+        res+=CalMemory(reinterpret_cast<koopa_raw_value_t>(ptr));
+        break;
+      default:
+        // 我们暂时不会遇到其他内容, 于是不对其做任何处理
+        assert(false);
+    }
+  }
+  return res;
+}
 // 这里的slice可以是各种level的代码片段, 从func到value, 然后对应不同的level调用不同的函数，这里是不同level进入更低level的借口，统一规约为slice然遍历执行
 void Visit(const koopa_raw_slice_t &slice, char * RiscV)
 {
